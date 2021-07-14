c2f2851ed7be1e6a32b653338a2ba4324886ecf7
amirmosio <amir.mn.mosio@gmail.com>
2021-07-14 23:28:23 +0430

    Debug CA model

--- src/CA/CA_model.py
+++ src/CA/CA_model.py
@@ -2,7 +2,6 @@ import os
 import random
 
 from OpenSSL import crypto
-from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.backends.openssl.backend import backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
@@ -15,27 +14,27 @@ class UserDataModel:
         self.public_cert_list = {}  # key: public key, value:certification
         # certification request state
         self.last_verification_code = None
-        self.last_public_key_cert_request = None
+        self.last_public_key_cert_request: str = None
 
 
 class CA:
     # crypto file addresses
     _ca_certificate_address = os.path.join(os.getcwd(), "ca_self_signed_cert.crt")
-    _ca_private_address = os.path.join(os.getcwd(), "ca_self_signed_cert.crt")
-    _ca_public_address = os.path.join(os.getcwd(), "ca_self_signed_cert.crt")
+    _ca_private_address = os.path.join(os.getcwd(), "private.key")
+    _ca_public_address = os.path.join(os.getcwd(), "public.key")
 
     @property
     def _public_key_object(self):
         return serialization.load_pem_public_key(
             self._pub_key.encode('utf-8'),
-            backend=default_backend()
+            backend=backend
         )
 
     @property
     def _private_key_object(self):
         return serialization.load_pem_private_key(
             self._pri_key.encode('utf-8'),
-            backend=default_backend(),
+            backend=backend,
             password=None
         )
 
@@ -63,13 +62,16 @@ class CA:
             )
         )
         # send encrypted verification mail
-        self._send_mail(gmail, encrypted_rand_string)
+        print(rand_string)
+        # self._send_mail(gmail, encrypted_rand_string)
 
         # update user model data
         user_data_model: UserDataModel = self._users_data.get(gmail, UserDataModel(gmail))
         self._users_data[gmail] = user_data_model
         user_data_model.last_verification_code = rand_string
         user_data_model.last_public_key_cert_request = user_public_key
+
+        return encrypted_rand_string
 
     def incoming_response_verification_code(self, gmail, verification_code):
         user_data_model: UserDataModel = self._users_data.get(gmail, None)
@@ -78,9 +80,9 @@ class CA:
                 requested_pub_key = user_data_model.last_public_key_cert_request
                 cert = self._sign_user_certificate(requested_pub_key, gmail)
                 user_data_model.public_cert_list[requested_pub_key] = cert
-                self._send_mail(gmail, cert)
-                return True
-        return False
+                # self._send_mail(gmail, cert)
+                return cert
+        return None
 
     def _send_mail(self, receiver_gmail, message):
         import smtplib, ssl
@@ -92,18 +94,19 @@ class CA:
         with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
             server.login(self._sender_mail, self._sender_password)
             server.sendmail(self._sender_mail, receiver_gmail, message)
+            print("sent to: ", receiver_gmail)
+            print("message:\n", message)
 
-    def _sign_user_certificate(self, user_public_key, user_mail):
-        ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM, open(self._ca_certificate_address).read())
-        ca_private_key = crypto.load_privatekey(crypto.FILETYPE_PEM,
-                                                open(os.path.join(os.getcwd(), "rsa.private")).read())
-        cert_req = crypto.X509Req()
+    def _sign_user_certificate(self, user_public_key: str, user_mail: str):
+        ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM, self._cert)
+        ca_private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, self._pri_key)
 
-        user_pem_pub_key = serialization.load_pem_public_key(user_public_key, backend)
+        user_pem_pub_key = serialization.load_pem_public_key(user_public_key.encode('utf-8'), backend)
         user_pkey_just_public = crypto.PKey()
         user_pkey_just_public = user_pkey_just_public.from_cryptography_key(user_pem_pub_key)
 
         #
+        cert_req = crypto.X509Req()
         cert_req.get_subject().C = "IR"
         cert_req.get_subject().ST = "state"
         cert_req.get_subject().L = "location"
@@ -143,13 +146,13 @@ class CA:
         cert.set_issuer(cert.get_subject())
         cert.set_pubkey(k)
         cert.sign(k, 'sha512')
-        with open("ca_self_signed_cert.crt", "wt") as f:
+        with open(self._ca_certificate_address, "wt") as f1:
             self_signed_cert_utf = crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8")
-            f.write(self_signed_cert_utf)
-        with open("ca_private.key", "wt") as f:
+            f1.write(self_signed_cert_utf)
+        with open(self._ca_private_address, "wt") as f2:
             private_key_utf = crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8")
-            f.write(private_key_utf)
-        with open("ca_public.key", "wt") as f:
+            f2.write(private_key_utf)
+        with open(self._ca_public_address, "wt") as f3:
             public_key_utf = crypto.dump_publickey(crypto.FILETYPE_PEM, cert.get_pubkey()).decode("utf-8")
-            f.write(public_key_utf)
+            f3.write(public_key_utf)
         return public_key_utf, private_key_utf, self_signed_cert_utf
